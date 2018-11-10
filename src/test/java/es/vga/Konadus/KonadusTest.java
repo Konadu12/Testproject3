a33e87ba6bd9881f9c43f544e1ccb3fd0b4c3408
amankwaah <amankwaah@192.168.0.46>
2018-11-10 20:18:45 +0100

    Aktuellste version

--- src/test/java/es/vga/Konadus/KonadusTest.java
+++ src/test/java/es/vga/Konadus/KonadusTest.java
@@ -1,14 +1,25 @@
 package es.vga.Konadus;
 
 import org.junit.Test;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Assert;
 
 public class KonadusTest {
+	private WebDriver driver;
+	String appURL = "www.google.com";
+	
+	public void testSetUp() {
+		
+		driver = new ChromeDriver();
+	}
 
 	@Test
 	public void dummyTest() {
 		Assert.assertTrue(true);
+		driver.navigate().to(appURL);
+		
+		
 		
 	}
 	
