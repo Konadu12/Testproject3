0e660c04b787fb453dc258afb1cb0582e97ef28c
amankwaah <amankwaah@192.168.0.46>
2018-11-10 14:28:25 +0100

    correct lo

--- src/main/java/es/vga/konadu/Konadu.java
+++ src/main/java/es/vga/konadu/Konadu.java
@@ -3,7 +3,7 @@ package es.vga.konadu;
 public class Konadu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
--- src/test/java/es/vga/Konadus/KonadusTest.java
+++ src/test/java/es/vga/Konadus/KonadusTest.java
@@ -1,5 +1,13 @@
 package es.vga.Konadus;
 
+import org.junit.Test;
+
+import org.junit.Assert;
+
 public class KonadusTest {
 
+	@Test
+	public void dummyTest() {
+		Assert.assertTrue(true);
+	}
 }
