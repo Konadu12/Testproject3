b63f48c5c502c35667ebb4cd29beadeab495bdb3
amankwaah <amankwaah@192.168.0.46>
2018-11-10 20:36:29 +0100

    akt

--- src/test/java/es/vga/Konadus/KonadusTest.java
+++ src/test/java/es/vga/Konadus/KonadusTest.java
@@ -1,8 +1,6 @@
 package es.vga.Konadus;
 
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.junit.Assert;
 
 public class KonadusTest {
@@ -11,9 +9,7 @@ public class KonadusTest {
 	@Test
 	public void dummyTest() {
 		Assert.assertTrue(true);
-		WebDriver driver = new SafariDriver();
-		driver.get("file:///Users/amankwaah/Desktop/website/vivid-photo/index.html");
-		driver.manage().window().maximize();
+		
 		
 	}
 	
