cf8772d003ac8eb70a80f08001764c705911dd5c
amankwaah <amankwaah@192.168.0.46>
2018-11-10 14:08:25 +0100

    commit inicial

---
+++ src/main/java/es/vga/konadu/Konadu.java
@@ -0,0 +1,10 @@
+package es.vga.konadu;
+
+public class Konadu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/es/vga/Konadus/KonadusTest.java
@@ -0,0 +1,5 @@
+package es.vga.Konadus;
+
+public class KonadusTest {
+
+}
